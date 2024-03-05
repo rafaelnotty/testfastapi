85efa17722b9385c25b3b975a7a0763e908f7a9b
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2024-03-05 15:49:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'Hello, World2!'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
