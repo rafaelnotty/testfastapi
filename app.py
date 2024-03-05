eae48619d564f103cc9f8f9bedd33d8b8b57fcc6
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2024-03-05 16:53:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return jsonify({'message': 'Hello, World2!'})
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
